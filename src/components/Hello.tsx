4bdb66a3cdca9486fe7565215d3941b3efc34cd6
kavish6 <kavishdadhich@gmail.com>
2024-04-01 17:26:45 +0530

    added new plugin

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,19 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 
 export interface HelloProps {
   name: string;
 }
 
 export const Hello: FunctionComponent<HelloProps> = ({ name }) => (
-  <h1>{ name }</h1>
+  <>
+    {" "}
+    <h1>{name}</h1>
+    <div>
+      FALALALAL
+      <div>
+        LALALALAL
+        <div>OOHLALALAL</div>
+      </div>
+    </div>
+  </>
 );
